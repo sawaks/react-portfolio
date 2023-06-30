3b72554d3fdf68b4d2926ec3c62702df95d3cedd
Sawako Goshima <sawako@192-168-1-103.tpgi.com.au>
2023-06-30 21:23:27 +1000

    Created AboutMe.js, Portfoliio.js, Resume.js, Contact.js at components folder

--- sawako-portfolio-react/src/App.js
+++ sawako-portfolio-react/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
+      <Portfolio />
+      <Contact />
+      <Resume />
+
     </div>
   );
 }
